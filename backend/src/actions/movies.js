97d7a8a93e3ab031affe0f36ceaf101cc8e2e68d
tame84 <146751584+tame84@users.noreply.github.com>
2024-09-29 17:45:41 +0200

    ancien code inutilse supprimé

--- backend/src/actions/movies.js
+++ backend/src/actions/movies.js
@@ -33,38 +33,3 @@ export const addMoviesToDatabase = async (movies) => {
         await client.end();
     }
 };
-
-// export const addMoviesToDatabase = async (movies) => {
-//     await client.connect();
-
-//     try {
-//         await client.query('DELETE FROM movies_schedules');
-//         await client.query('DELETE FROM movies');
-
-//         for (const { title, posterURL, duration, schedules, genre, actors, director, synopsis, cinemaId } of movies) {
-//             const movieSQLValue = [title, posterURL, duration, genre, actors, director, synopsis, cinemaId];
-
-//             await client.query(
-//                 'INSERT INTO movies (title, poster_url, duration, genre, actors, director, synopsis, cinema_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)',
-//                 movieSQLValue
-//             );
-//             const res = await client.query('SELECT id FROM movies WHERE title = $1', [title]);
-//             const movieId = res.rows[0].id;
-
-//             for (const { date, hours } of schedules) {
-//                 const schedulesSQLValue = [date, JSON.stringify(hours), movieId];
-
-//                 await client.query(
-//                     'INSERT INTO movies_schedules (date, hours, movie_id) VALUES ($1, $2, $3)',
-//                     schedulesSQLValue
-//                 );
-//             }
-//         }
-
-//         console.log('Movies are now in the database.');
-//     } catch (error) {
-//         console.error('Database Error :', error);
-//     } finally {
-//         await client.end();
-//     }
-// };
