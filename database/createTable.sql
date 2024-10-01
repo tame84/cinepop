cb7c6afe5cd56356950115c6b8e4f09082b689a0
tame84 <146751584+tame84@users.noreply.github.com>
2024-10-01 18:56:17 +0200

    modification de la structure de la base de donnée (notamment la suppression de la table cinemas)

--- database/createTable.sql
+++ database/createTable.sql
@@ -1,29 +1,23 @@
 DROP TABLE IF EXISTS movies_schedules;
 DROP TABLE IF EXISTS movies;
-DROP TABLE IF EXISTS cinema;
-
-CREATE TABLE IF NOT EXISTS cinema (
-    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    url VARCHAR(255) NOT NULL
-);
 
 CREATE TABLE IF NOT EXISTS movies (
     uuid UUID PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
     title VARCHAR(255) NOT NULL,
-    poster_url VARCHAR(255) NOT NULL,
-    duration SMALLINT NOT NULL,
-    genre JSONB,
-    actors JSONB,
-    director JSONB,
+    duration SMALLINT,
+    genre VARCHAR(255),
+    director JSON,
+    release_date DATE,
+    original_language VARCHAR(255),
+    country VARCHAR(255),
+    casting JSON,
     synopsis TEXT,
-    cinema_id INTEGER NOT NULL,
-    FOREIGN KEY (cinema_id) REFERENCES cinema(id)
+    poster JSON NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS movies_schedules (
     date DATE NOT NULL,
-    hours JSONB NOT NULL,
-    movie_id UUID NOT NULL,
-    FOREIGN KEY (movie_id) REFERENCES movies(uuid)
+    schedules JSON NOT NULL,
+    movie_uuid UUID NOT NULL,
+    FOREIGN KEY (movie_uuid) REFERENCES movies(uuid)
 );
