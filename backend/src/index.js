db18b20d179f0f10a029ec7cf7f2a72c92d0187d
tame84 <146751584+tame84@users.noreply.github.com>
2024-10-01 19:43:24 +0200

    fix, les fonctions dans index.js ne se trouvaient pas dans le cron

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,9 +2,9 @@ import cron from 'node-cron';
 import scrapeMovies from './scrape.js';
 import { addToDatabase } from './actions/movies.js';
 
-cron.schedule('0 0 * * *', async () => {});
-
-console.log('Daily scraping started...');
-const movies = await scrapeMovies();
-await addToDatabase(movies);
-console.log('Daily scraping finished.');
+cron.schedule('0 0 * * *', async () => {
+    console.log('Daily scraping started...');
+    const movies = await scrapeMovies();
+    await addToDatabase(movies);
+    console.log('Daily scraping finished.');
+});
